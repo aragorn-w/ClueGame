3b98d72ed4d80d2ab020b7d73d14db848272fb96
Aragorn W <wangaragorn@gmail.com>
2025-02-25 13:45:08 -0700

    Updated board tests

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,5 +1,30 @@
 package tests;
 
-public class BoardTestsExp {
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
+import experiment.TestBoard;
+
+public class BoardTestsExp {
+	private TestBoard board;
+	
+	@BeforeEach
+	public void setUp() {
+		board = new TestBoard();
+	}
+	
+	@Test
+	public void testAdjacency() {
+		
+	}
+	
+	@Test
+	public void testMakeTargetsNormal() {
+		
+	}
+	
+	@Test
+	public void testMakeTargetsSpecial() {
+		
+	}
 }
