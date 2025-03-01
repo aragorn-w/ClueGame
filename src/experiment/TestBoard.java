0272eb476606a974a5a5727b6a501152f9d30e61
Anya Streit <anya.streit@gmail.com>
2025-02-28 22:58:57 -0700

    Added JUnit tests

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -8,8 +8,19 @@ public class TestBoard {
 	private ArrayList<ArrayList<TestBoardCell>> board = new ArrayList<ArrayList<TestBoardCell>>();
 	private Set<TestBoardCell> targets = new HashSet<>();
 	
+	private int NUM_ROWS = 4;
+    private int NUM_COLS = 4;
+	
 	public TestBoard() {
 		super();
+
+	    for (int i = 0; i < NUM_ROWS; i++) {
+	        ArrayList<TestBoardCell> row = new ArrayList<>();
+	        for (int j = 0; j < NUM_COLS; j++) {
+	            row.add(new TestBoardCell(i, j));  // Assuming TestBoardCell has a constructor
+	        }
+	        board.add(row);
+	    }
 	}
 	
 	public void calcTargets(TestBoardCell startCell, int pathLength) {
